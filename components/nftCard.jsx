18bd429df07aa6ff641f97b528622e3e37611095
zhugaopeng397 <zhugaopeng397@zhugaopeng397deMacBook-Pro.local>
2022-09-07 16:44:38 +0800

    init

---
+++ components/nftCard.jsx
@@ -0,0 +1,27 @@
+export const NFTCard = ({ nft }) => {
+
+    return (
+        <div className="w-1/4 flex flex-col ">
+        <div className="rounded-md">
+            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        </div>
+        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+            <div className="">
+                <h2 className="text-xl text-gray-800">{nft.title}</h2>
+                <p className="text-gray-600">Token Id: {nft.id.tokenId}</p>
+                <p className="text-gray-600" >Contract Address: {nft.contract.address}</p>
+                <div className="hover:bg-violet-200" onClick={() => {navigator.clipboard.writeText(nft.contract.address)}}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+                </svg>
+                </div>
+            </div>
+
+            <div className="flex-grow mt-2">
+                <p className="text-gray-600">Description: {nft.description}</p>
+            </div>
+        </div>
+
+    </div>
+    )
+}
